a513efcb075dd7e18cb86b108de662002e4243ba
jelizaveta890-prog <jelizaveta890@gmail.com>
2025-10-31 10:54:34 +0200

    Switch

---
+++ SwichMultipleCall/SwichMultipleCall/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SwichMultipleCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //teha switch rakendus,
+            //kus kasutaja sisestab sõiduki tüübi (coupe või truck)
+            string input = Console.ReadLine();
+            //ja programm kutsub vastava meetodi (CarCope või Truck)
+            switch (input)
+            {
+                case "Car Coupe":
+                    CarCoupe();
+
+                    break;
+
+                case "Truck":
+                    Truck();
+                    break;
+
+                default:
+                    Console.WriteLine("Unknown vehicle type.");
+                    break;
+            }
+
+            static void CarCoupe()
+            {
+                Console.WriteLine("This is a coupe car.");
+            }
+
+            static void Truck()
+            {
+                Console.WriteLine("This is a truck.");
+            }
+        }
+    }
+}
---
+++ SwichMultipleCases/SwichMultipleCases/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SwichMultipleCases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //sisestad konsooli väärtuse
+            //loeb sisestatud väärtuse stringina
+            string input = Console.ReadLine();
+
+            //tuleb kasutada switchi
+            switch (input)
+            {
+
+
+             //esimeses cases on väärtused "Intro to C#", "Variables", 
+             //"Data Types" ja vastuseks Basic 
+             case "Intro to C#" or "Variables" or "Data Types":
+                    Console.WriteLine("Basic");
+                    break;
+             
+             //teises cases on väärtused "OOP"', "Classes",
+             //"Objects" ja vastuseks Intermediate
+                case "OOP" or "Classes" or "Objects":
+                    Console.WriteLine("Intermediate");
+                    break;
+             
+             //kolmandas cases on väärtused "Asynchronous Programming", "LINQ".
+             //"Delegates" ja vastuseks Advanced
+                case "Asynchronous Programming" or "LINQ" or "Delegates":
+                    Console.WriteLine("Advanced");
+                    break;
+                
+                default:
+                    Console.WriteLine("Unknown course");
+                  break;
+            }
+        }
+    }
+}
