9fff7130cf55bd6526220767b55c6e4fc9dcca71
Opilane TTHK <opilane@tthk.ee>
2025-11-14 09:32:02 +0200

    SwitchCalculator

---
+++ SwitchCalculator/SwitchCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SwitchCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("esineme number");
+            float firstNumber = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sisesta tehe");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("teine number");
+            float secondNumber = float.Parse(Console.ReadLine());
+
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine(firstNumber + secondNumber);
+                    break;
+
+                case "-":
+                    Console.WriteLine(firstNumber - secondNumber);
+                    break;
+                case "*":
+                    Console.WriteLine(firstNumber * secondNumber);
+                    break;
+
+                case "/":
+                    Console.WriteLine(firstNumber / secondNumber);
+                    break;
+                default:
+                    Console.WriteLine("Vale operaator");
+                    break;
+            }
+        }
+    }
+}
