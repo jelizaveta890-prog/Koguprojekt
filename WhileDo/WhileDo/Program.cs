f4757f186a2764c59b2d95b0c4311f3eb913d5a3
Opilane TTHK <opilane@tthk.ee>
2025-11-21 10:27:05 +0200

    WhileDo

---
+++ WhileDo/WhileDo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace WhileDo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("While Do");
+
+            int i = 1;
+
+            do
+            {
+
+                Console.WriteLine(i);
+                //suurendab numbrit ühe võrra e number + 1
+                i++;
+                //Kui number on väiksem voi võrdne 5-ga siis jätkab tsüklit
+            } while (i <= 10);
+        }
+    }
+}
