d758d6d9285715f6dc016ba602131d255cf8acea
Opilane TTHK <opilane@tthk.ee>
2025-11-21 11:04:37 +0200

    WhileDoTriangle

---
+++ WhileDoTriangle/WhileDoTriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace WhileDoTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kolmnurk");
+
+            int rows = int.Parse(Console.ReadLine()); // The desired number of rows
+            int i = 1;
+            while (i < rows)
+            {
+                int j = 1;
+                while (j <= i)
+                {
+                    Console.Write(j + " ");
+                    j++;
+                }
+                Console.WriteLine();
+                i++;
+
+
+                //otsida Internetist While koodiga kolmnurga kujundeid e tegemise viisi
+                //kui kood on leitud, siis kirjutada seda siia alla
+                //teha vooskeem
+
+
+            }
+        }
+    }
+}
