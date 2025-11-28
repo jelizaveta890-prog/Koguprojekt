cc4ec8fa6972927618b9dfc61bd2f5352ac033f1
Opilane TTHK <opilane@tthk.ee>
2025-11-28 11:03:39 +0200

    KujundiKalkulaator

---
+++ KujundiKalkulaator/KujundiKalkulaator/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace KujundiKalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+      
+        //Ruudu sees asub ring. Ringi raadius on 3 ühikut.
+        //Leia ja väljasta ekraanile ruudu pindala ja ümbermõõt.
+        
+            int roundRadius = 3;
+            int diameter = roundRadius * 2;
+            double squareArea = diameter * diameter;
+            double squarePerimeter = 4 * diameter;
+
+            
+            Console.WriteLine("Ruudu pindala:" + squareArea);
+            Console.WriteLine("Ruudu ümbermõõt:" + squarePerimeter);
+
+       
+
+
+            //arvutada ringi pindala ja ümbermõõt
+            double roundArea = Math.PI * (roundRadius * roundRadius);
+            double roundPerimeter = 2 * Math.PI * roundRadius;
+
+
+            Console.WriteLine("Ringi pindala:" + roundArea);
+            Console.WriteLine("Ringi ümbermõõt:" + roundPerimeter);
+        }
+    }
+}
