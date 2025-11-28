9cc47d8473eb2d0b558de1a0109e208ad1dec952
Opilane TTHK <opilane@tthk.ee>
2025-11-28 09:47:38 +0200

    ForLoop

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Loop");
+
+            int loop = 10;
+
+            //int i on muutuja määratlemin ja selle väärtus on 0
+            // i < loop iga kord võrdleb loop muutja, et kas on suurem i-st
+            //i++ liidab iga kord ühe korra juurde e sama hea, kui i + 1
+            //teha vooskeem
+
+            for (int i = 0; i < loop; i++) 
+            {
+                Console.WriteLine("Loop" + i);
+                Thread.Sleep(1000);
+                Console.Beep();
+            }
+        }
+    }
+}
