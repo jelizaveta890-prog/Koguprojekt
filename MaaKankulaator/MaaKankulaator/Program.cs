2e284adc77bedc6373bafb69322e05fb7d471a43
Opilane TTHK <opilane@tthk.ee>
2025-11-28 11:25:17 +0200

    MaaKankulaator

---
+++ MaaKankulaator/MaaKankulaator/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MaaKankulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Meil on münt, mille diameeter on 25.75 mm
+            //Arvutada välja Maa ümbermõõt ja mitu münti
+            //Mahub järjestikku ümber Maa
+            //Arvutuses tohib kasutada ainult mündi diameeti ja Maa raadiust
+
+            //raadius = 6371 km,
+
+            double coinDiameter = 25.75; //mm
+            double earthRadius = 637100000;
+            double aroundEarth = 2 * Math.PI * earthRadius;
+
+            Console.WriteLine(aroundEarth);
+            double howManyCoins = aroundEarth / coinDiameter;
+            Console.WriteLine("Kahe euroseid läheb ümber maa {0:0}", howManyCoins);
+
+
+
+
+        }
+    }
+}
