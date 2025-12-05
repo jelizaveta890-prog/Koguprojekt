5cb9a097caa126d96eecd87fdde151bfbc699b53
Opilane TTHK <opilane@tthk.ee>
2025-12-05 11:01:17 +0200

    ForLoopAdding

---
+++ ForLoopAdding/ForLoopAdding/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ForLoopAdding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Täisarvude kokku liitmine kuni kümneni"); 
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("\n\n");
+
+            int sum = 0;
+
+            for (int i = 0; i <= 10; i++)
+            {
+                sum += i;
+                Console.WriteLine("Summa on: " + sum);
+            }
+        }
+    }
+}
