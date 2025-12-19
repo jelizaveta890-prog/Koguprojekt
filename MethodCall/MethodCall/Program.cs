40547531d2a2bc6ecc3ec47288d496a6bd369f42
Opilane TTHK <opilane@tthk.ee>
2025-12-19 11:01:06 +0200

    MethodCall

---
+++ MethodCall/MethodCall/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MethodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            SayHello();
+            SayGoodbye();
+        }
+   
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void SayGoodbye()
+        {
+            Console.WriteLine("SayGoodbye");
+        }
+
+
+
+
+    }
+}
