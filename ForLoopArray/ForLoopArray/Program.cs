3f336eb21595f152bf300a272873eb7c244b5b62
Opilane TTHK <opilane@tthk.ee>
2026-01-05 09:46:27 +0200

    ForLoopArray

---
+++ ForLoopArray/ForLoopArray/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ForLoopArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //vaja teha string array H, e, l, l, o
+            //kasutada for loopi
+            string[] array = { "H", "e", "l", "l", "o" };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+
+            }
+           
+            
+            //kasutage nüüd foreach-i
+            foreach (var i in array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
