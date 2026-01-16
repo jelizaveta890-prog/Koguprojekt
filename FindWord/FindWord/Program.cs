0d350ddcc920f4c832239d0b050495f4e6801d76
Opilane TTHK <opilane@tthk.ee>
2026-01-16 11:28:09 +0200

    FindWord

--- FindWord/FindWord/Program.cs
+++ FindWord/FindWord/Program.cs
@@ -23,6 +23,7 @@ namespace FindWord
               RegexOptions.Compiled |   RegexOptions.IgnoreCase);
             try 
             {
+                //faili asukohta peab muutma 
                 using (StreamReader sr = new StreamReader("C:/Users/opilane/Desktop/lemmad.txt", Encoding.Default))
                 {
                    int lineNumber = 0; 
