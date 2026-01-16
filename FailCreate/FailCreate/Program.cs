4f4369ab4b4870b330689d58dad43cec7d16ac4e
Opilane TTHK <opilane@tthk.ee>
2026-01-16 10:43:40 +0200

    FailCreate

---
+++ FailCreate/FailCreate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Channels;
+
+namespace FailCreate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjutab faili läbi konsooli");
+
+            string filePath = "C:/Users/opilane/Desktop/WriteToFile.txt";
+            string input = Console.ReadLine();
+
+            File.WriteAllText(filePath, input);
+       
+        
+        
+        }
+    }
+}
